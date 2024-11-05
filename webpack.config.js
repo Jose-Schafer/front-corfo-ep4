4b46f413d7be97e89c71ef964f9befc707b1566c
Jose Schafer <joseignacio.schafer@gmail.com>
2024-11-05 08:13:11 -0300

    Solucionado warnings por imports

--- webpack.config.js
+++ webpack.config.js
@@ -5,50 +5,90 @@ const autoprefixer = require('autoprefixer')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-	mode: 'development',
-	entry: './src/js/main.js',
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	devServer: {
-		static: path.resolve(__dirname, 'dist'),
-		port: 8080,
-		hot: true
-	},
-	plugins: [
-		new HtmlWebpackPlugin({ template: './src/index.html' })
-	],
-	module: {
-		rules: [
-			{
-				test: /\.(scss)$/,
-				use: [
-					{
-						// Adds CSS to the DOM by injecting a `<style>` tag
-						loader: 'style-loader'
-					},
-					{
-						// Interprets `@import` and `url()` like `import/require()` and will resolve them
-						loader: 'css-loader'
-					},
-					{
-						// Loader for webpack to process CSS with PostCSS
-						loader: 'postcss-loader',
-						options: {
-							postcssOptions: {
-								plugins: [
-									autoprefixer
-								]
-							}
-						}
-					},
-					{
-						// Loads a SASS/SCSS file and compiles it to CSS
-						loader: 'sass-loader'
-					}
-				]
-			}
-		]
-	}
+  mode: 'development',
+  entry: './src/js/main.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: 'img/[name].[ext]'
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'equipo.html',
+      template: 'src/equipo.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'contacto.html',
+      template: 'src/contacto.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer
+                ]
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                silenceDeprecations: ['mixed-decls', 'color-functions', 'global-builtin', 'import'],
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|pdf|ico)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "img/[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ]
+  }
 }
